1065404d92707d3c2147850c3c9c356603d6c8cc
lcy <yohostarrynight@gmail.com>
2020-05-18 09:48:05 +0800

    redirect

--- src/axios_config/axios_config.js
+++ src/axios_config/axios_config.js
@@ -15,7 +15,7 @@ axios.interceptors.response.use(response => {
 },
 err => {
   if (err.response.status === 401) {
-    window.location.href = '/login';
+    window.location.href = '/';
   }
   return Promise.reject(err);
 });
